91347dd4455d424bc9b87b2b483754e05260790e
kth701 <kth701@nate.com>
2025-01-23 10:40:39 +0900

    - 첫번째 내용 한줄 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        System.out.println("첫번째 commit한 내용 ");
     }
 }
